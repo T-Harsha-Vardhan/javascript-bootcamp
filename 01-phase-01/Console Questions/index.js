0236ad128bb80efe5e64b0e2b375270334e88d8c
Farhan-Khan7 <fk5518744@gmail.com>
2026-07-02 23:59:34 +0530

    I have done console questions

---
+++ 01-phase-01/Console Questions/index.js
@@ -0,0 +1,12 @@
+let name = "Farhan";
+let age = 19;
+let city = "Bhopal";
+
+console.log(name , age , city);
+
+
+console.warn("You are fired!");
+
+console.error("Your are blocked!");
+
+console.table(["Farhan khan" , 19 , "Software Engineer" , "Adobe" , "Saas builder"])
